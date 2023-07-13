ffd705a2820b63e82bd8edcfe4c8d717d515388c
DipeshGoud <112858278+DipeshGoud@users.noreply.github.com>
2023-07-13 08:54:11 +0000

    submit

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,27 +1,25 @@
 import React, { useState } from "react";
 
 function Input() {
-    const [name, setName] = useState('');
-    const [greeting, setGreeting] = useState('');
+  const [name, setName] = useState("");
+  const [greeting, setGreeting] = useState("");
 
-    function handleChange(event) {
-        setName(event.target.value);
-        setGreeting(`Hello ${name}!`);
-    }
+  function handleChange(event) {
+    setName(event.target.value);
+    setGreeting(`Hello ${name}!`);
+  }
 
-    return (
-        <div>
-            <p>Enter your name:</p>
-            <form>
-                <label>
-                    Enter your name:
-                    <input type="text" value={name} onChange={handleChange} />
-                </label>
-            </form>
-            <p>{greeting}</p>
-        </div>
-    );
+  return (
+    <div>
+      <form>
+        <label>
+          Enter your name:
+          <input type="text" value={name} onChange={handleChange} />
+        </label>
+      </form>
+      <p>{greeting}</p>
+    </div>
+  );
 }
-
 
 export default Input;

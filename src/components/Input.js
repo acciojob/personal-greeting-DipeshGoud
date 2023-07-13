ef86e6ee07aa32782d7fc756ad97428677954256
DipeshGoud <112858278+DipeshGoud@users.noreply.github.com>
2023-07-13 08:45:05 +0000

    submit

---
+++ src/components/Input.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+function Input() {
+    const [name, setName] = useState('');
+    const [greeting, setGreeting] = useState('');
+
+    function handleChange(event) {
+        setName(event.target.value);
+        setGreeting(`Hello ${name}!`);
+    }
+
+    return (
+        <div>
+            <form>
+                <label>
+                    Enter your name:
+                    <input type="text" value={name} onChange={handleChange} />
+                </label>
+            </form>
+            <p>{greeting}</p>
+        </div>
+    );
+}
+
+
+export default Input;
